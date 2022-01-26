f60dd2369c62b6f342f6ace28556ed5ee8e5c0a1
Dominik-Pawlowski <dominik.j.pawlowski@student.put.poznan.pl>
2022-01-26 22:16:35 +0100

    Added more documentation

--- src/main/java/pl/put/poznan/transformer/Visitors/ActorChecker.java
+++ src/main/java/pl/put/poznan/transformer/Visitors/ActorChecker.java
@@ -4,31 +4,57 @@ import pl.put.poznan.transformer.Scenario.*;
 
 import java.util.ArrayList;
 
-
+/**
+ * The ActorChecker class checks whether
+ * every actor declared to be in the scenario is actually used.
+ */
 public class ActorChecker implements ScenarioPartVisitor {
 
+    /**
+     * Stores an ArrayList of unused actors.
+     */
     private ArrayList<String> unusedActors = new ArrayList<>();
 
+    /**
+     * Unused.
+     * @param scenario Scenario object
+     */
     @Override
     public void visit(Scenario scenario) {
 
     }
 
+    /**
+     * Unused.
+     * @param title Title object
+     */
     @Override
     public void visit(Title title) {
 
     }
 
+    /**
+     * Puts every actor name inside ArrayList unusedActors
+     * @param actor Actor object
+     */
     @Override
     public void visit(Actor actor) {
         unusedActors.add(actor.getName());
     }
 
+    /**
+     * Puts every system actor name inside ArrayList unusedActors
+     * @param systemActor SystemActor object
+     */
     @Override
     public void visit(SystemActor systemActor) {
         unusedActors.add(systemActor.getName());
     }
 
+    /**
+     * Checks if any of the actors are a part of a step
+     * @param step Step object
+     */
     @Override
     public void visit(Step step) {
         for(String act : unusedActors){
@@ -39,6 +65,10 @@ public class ActorChecker implements ScenarioPartVisitor {
         }
     }
 
+    /**
+     * Standard getter method.
+     * @return String containing all unused actors' names
+     */
     public String getUnused(){
         String un = "";
         for (String act : unusedActors){
--- src/main/java/pl/put/poznan/transformer/Visitors/KeywordCounter.java
+++ src/main/java/pl/put/poznan/transformer/Visitors/KeywordCounter.java
@@ -2,12 +2,27 @@ package pl.put.poznan.transformer.Visitors;
 
 import pl.put.poznan.transformer.Scenario.*;
 
+/**
+ * The KeywordCounter class counts every use of IF, ELSE and FOR EACH
+ * in a given scenario.
+ */
 public class KeywordCounter implements ScenarioPartVisitor {
+    /**
+     * Number of keywords found
+     */
     private int keywordCount = 0;
 
+    /**
+     * Unused.
+     * @param scenario Scenario object
+     */
     @Override
     public void visit(Scenario scenario) {}
 
+    /**
+     * Checks if given step name contains one of valid keywords.
+     * @param step Step object
+     */
     @Override
     public void visit(Step step) {
         String text = step.getStep().trim();
@@ -22,16 +37,32 @@ public class KeywordCounter implements ScenarioPartVisitor {
         }
     }
 
+    /**
+     * Standard getter method.
+     * @return Number of found keywords in a scenario
+     */
     public int getKeywordCount() {
         return this.keywordCount;
     }
 
+    /**
+     * Unused.
+     * @param title Title object
+     */
     @Override
     public void visit(Title title) {}
 
+    /**
+     * Unused.
+     * @param actor Actor object
+     */
     @Override
     public void visit(Actor actor) {}
 
+    /**
+     * Unused.
+     * @param systemActor SystemActor object
+     */
     @Override
     public void visit(SystemActor systemActor) {}
 }
